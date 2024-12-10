f313189a566ce7ac0eed048dc1baef1e2856ecf9
ola567 <aleksandra.hein@o2.pl>
2024-12-10 23:04:18 +0100

    default.cu

--- cuda_streams/default.cu
+++ cuda_streams/default.cu
@@ -2,90 +2,84 @@
 #include <stdlib.h>
 #include <cuda_runtime.h>
 
-__host__ void errorexit(const char *s)
-{
-    printf("\n%s", s);
-    exit(EXIT_FAILURE);
+__host__
+void errorexit(const char *s) {
+    printf("\n%s",s);	
+    exit(EXIT_FAILURE);	 	
 }
 
-__global__ void computeHistogram(int *randomNumbersDevice, unsigned long long *resultArrayDevice, int N, int A, int B)
-{
+__global__ void computeHistogram(int *randomNumbersDevice, unsigned long long *resultArrayDevice, int N, int A, int B) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < N)
-    {
+    if (idx < N) {
         int resultIdx = (randomNumbersDevice[idx] - A);
         atomicAdd(&resultArrayDevice[resultIdx], 1);
     }
 }
 
-void generateRandomNumbers(int *arr, int N, int A, int B)
-{
-    srand(time(NULL));
-    for (int i = 0; i < N; i++)
-    {
+void generateRandomNumbers(int *arr, int N, int A, int B) {
+	srand(time(NULL));
+    for (int i = 0; i < N; i++) {
         arr[i] = A + rand() % (B - A);
     }
+
 }
 
-int main(int argc, char **argv)
-{
-    int threadsinblock = 1024;
+int main(int argc,char **argv) {
+    int threadsinblock=1024;
     int blocksingrid;
 
     int N, A, B;
-
-    cudaEvent_t start, stop;
+    
+ 	cudaEvent_t start, stop;
     float milliseconds = 0;
-
+    
     printf("Enter number of elements: \n");
     scanf("%d", &N);
 
-    printf("Enter A value (start range): \n");
+	printf("Enter A value (start range): \n");
     scanf("%d", &A);
 
     printf("Enter B value (end range): \n");
     scanf("%d", &B);
 
-    int *randomNumbers = (int *)malloc(N * sizeof(int));
-    if (randomNumbers == NULL)
-    {
+	int *randomNumbers = (int *)malloc(N * sizeof(int));
+    if (randomNumbers == NULL) {
         printf("Memory allocation failed.\n");
         return 1;
     }
 
-    generateRandomNumbers(randomNumbers, N, A, B);
+	generateRandomNumbers(randomNumbers, N, A, B);
 
-    blocksingrid = ceil((double)N / threadsinblock);
+	blocksingrid = ceil((double) N / threadsinblock);
 
-    printf("The kernel will run with: %d blocks\n", blocksingrid);
+	printf("The kernel will run with: %d blocks\n", blocksingrid);
 
-    unsigned long long *resultArrayHost, *resultArrayDevice;
+	unsigned long long *resultArrayHost, *resultArrayDevice;
     int *randomNumbersDevice;
 
-    resultArrayHost = (unsigned long long *)calloc((B - A), sizeof(unsigned long long));
+	resultArrayHost = (unsigned long long *)calloc((B-A), sizeof(unsigned long long));
 
-    if (resultArrayHost == NULL)
-    {
+	if (resultArrayHost == NULL) {
         printf("Memory allocation failed.\n");
         return 1;
     }
 
-    cudaEventCreate(&start);
+	cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    cudaMalloc((void **)&randomNumbersDevice, N * sizeof(int));
-    cudaMalloc((void **)&resultArrayDevice, (B - A) * sizeof(unsigned long long));
+	cudaMalloc((void **)&randomNumbersDevice, N * sizeof(int));
+    cudaMalloc((void **)&resultArrayDevice, (B-A) * sizeof(unsigned long long));
 
     cudaMemcpy(randomNumbersDevice, randomNumbers, N * sizeof(int), cudaMemcpyHostToDevice);
 
     // Initialize device histogram to 0
-    cudaMemset(resultArrayDevice, 0, (B - A) * sizeof(unsigned long long));
+    cudaMemset(resultArrayDevice, 0, (B-A) * sizeof(unsigned long long));
 
     computeHistogram<<<blocksingrid, threadsinblock>>>(randomNumbersDevice, resultArrayDevice, N, A, B);
 
     // Copy the histogram result back to the host
-    cudaMemcpy(resultArrayHost, resultArrayDevice, (B - A) * sizeof(unsigned long long), cudaMemcpyDeviceToHost);
+    cudaMemcpy(resultArrayHost, resultArrayDevice, (B-A) * sizeof(unsigned long long), cudaMemcpyDeviceToHost);
 
     cudaEventRecord(stop, 0);
 
@@ -100,15 +94,14 @@ int main(int argc, char **argv)
     //     printf("%d, ", randomNumbers[i]);
     // }
     // printf("\n");
-
+    
     printf("Histogram:\n");
-    for (int i = 0; i < B - A; i++)
-    {
+    for (int i = 0; i < B-A; i++) {
         printf("%d occures %llu\n", i + A, resultArrayHost[i]);
     }
 
     // Print execution time
-    printf("Kernel execution time: %.3f ms\n", milliseconds);
+	printf("Kernel execution time: %.3f ms\n", milliseconds);
 
     // Free allocated memory
     free(randomNumbers);
